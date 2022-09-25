ce0d4bab11a2bc04ec78252d22873421b8409f13
LJM12914 <LJM12914@outlook.com>
2022-09-25 09:44:09 +0800

    0.1(e)
    dynamic与freeDOM的开发已中止，两项目将转为私有。

--- src/dynamic.export.ts
+++ src/dynamic.export.ts
@@ -5,29 +5,33 @@
 import * as utils from "../../utils/index";
 import * as lUtils from "./utils/index";
 
-//分文件开发
-import App from "./app";
-import * as template from "./template";
-import * as spa from "./spa";
-import * as manifest from "./manifest";
+const version = "1.0.0";
+//开发模式
+console.info(
+    `dynamic(dnJS) v${version} ©LJM12914. https://github.com/wheelsmake/dynamic
+    You are using the unminified build of dynamic. Make sure to use the minified build for production.`);
+
+//导入所有内容
+//import App from "./app";
 
 //构造导出对象
-const Dynamic$ = App as Dynamic;
-    //引入模块
-Dynamic$.template = template;
-Dynamic$.spa = spa;
-Dynamic$.manifest = manifest;
-    //工具方法
-Dynamic$.e = utils.element.e;
-Dynamic$.render = (args :{
-    HTML :string | Element | HTMLCollection | Element[] | Node | NodeList | Node[];
-    element :Element, insertAfter? :boolean, append? :boolean
-}) :Node[]=>{return utils.element.render(args.HTML, args.element, args.insertAfter, args.append);};
-Dynamic$.toHTML = utils.element.toHTML;
-Dynamic$.hatch = utils.element.hatch;
-Dynamic$.compose = ()=>{}; //todo:
+//const Dynamic$ = App as Dynamic;
+//    //引入模块
+//Dynamic$.template = template;
+//Dynamic$.spa = spa;
+//Dynamic$.manifest = manifest;
+//    //工具方法
+//Dynamic$.e = utils.element.e;
+//Dynamic$.render = (args :{
+//    HTML :string | Element | HTMLCollection | Element[] | Node | NodeList | Node[];
+//    element :Element, insertAfter? :boolean, append? :boolean
+//}) :Node[]=>{return utils.element.render(args.HTML, args.element, args.insertAfter, args.append);};
+//Dynamic$.toHTML = utils.element.toHTML;
+//Dynamic$.hatch = utils.element.hatch;
+//Dynamic$.compose = ()=>{}; //todo:
 
 //导出
-const Dynamic :Dynamic = Dynamic$;
-utils.generic.constantize(Dynamic);
+//const Dynamic :Dynamic = Dynamic$;
+const Dynamic = "";
+//utils.generic.constantize(Dynamic);
 export default Dynamic;
---
+++ src/render.ts
@@ -0,0 +1,10 @@
+﻿/* dynamic
+ * ©2022 LJM12914. https://github.com/wheelsmake/dynamic
+ * Licensed under MIT License. https://github.com/wheelsmake/dynamic/blob/main/LICENSE
+*/
+import * as utils from "../../utils/index";
+import * as lUtils from "./utils/index";
+
+export function render(){
+    
+}
